e47c1a64918a88f978a002b996acdad5f832d9ec
Xing Zhou <xing@ohdyno.me>
2016-10-31 20:28:26 -0700

    lesson 2 of TDD with Kent Beck with PragProg

--- src/test/TyrantMap.java
+++ src/test/TyrantMap.java
@@ -5,15 +5,18 @@ import java.net.Socket;
 
 class TyrantMap {
 
-    private static final int OPERATON_PREFIX = 0xC8;
+    private static final int OPERATION_PREFIX = 0xC8;
     private static final int PUT_OPERATION = 0x10;
     private static final int GET_OPERATION = 0x30;
+    private static final int VANISH_OPERATION = 0x72;
+    private static final int REMOVE_OPERATION = 0x20;
+    private static final int SIZE_OPERATION = 0x80;
     private Socket socket;
     private DataOutputStream writer;
     private DataInputStream reader;
 
     void put(byte[] key, byte[] value) throws IOException {
-        writer.write(OPERATON_PREFIX);
+        writer.write(OPERATION_PREFIX);
         writer.write(PUT_OPERATION);
         writer.writeInt(key.length);
         writer.writeInt(value.length);
@@ -25,17 +28,48 @@ class TyrantMap {
     }
 
     byte[] get(byte[] key) throws IOException {
-        writer.write(OPERATON_PREFIX);
+        writer.write(OPERATION_PREFIX);
         writer.write(GET_OPERATION);
         writer.writeInt(key.length);
         writer.write(key);
         int status = reader.read();
+        if (status == 1)
+            return null;
         if (status != 0)
             throw new RuntimeException();
         int length = reader.readInt();
         byte[] results = new byte[length];
         reader.read(results);// TODO read longer values
         return results;
+    }
+
+    void clear() throws IOException {
+        writer.write(OPERATION_PREFIX);
+        writer.write(VANISH_OPERATION);
+        int status = reader.read();
+        if (status != 0)
+            throw new RuntimeException();
+    }
+
+    void remove(byte[] key) throws IOException {
+        writer.write(OPERATION_PREFIX);
+        writer.write(REMOVE_OPERATION);
+        writer.writeInt(key.length);
+        writer.write(key);
+        int status = reader.read();
+        if (status == 1)
+            return;
+        if (status != 0)
+            throw new RuntimeException();
+    }
+
+    long size() throws IOException {
+        writer.write(OPERATION_PREFIX);
+        writer.write(SIZE_OPERATION);
+        int status = reader.read();
+        if (status != 0)
+            throw new RuntimeException();
+        return reader.readLong();
     }
 
     void open() throws IOException {
@@ -47,5 +81,4 @@ class TyrantMap {
     void close() throws IOException {
         socket.close();
     }
-
 }
--- src/test/TyrantTest.java
+++ src/test/TyrantTest.java
@@ -5,10 +5,18 @@ import org.junit.Test;
 import java.io.*;
 
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class TyrantTest {
 
     private TyrantMap tyrant;
+
+    @Test
+    public void getReturnsNullIfKeyNotFound() throws Exception {
+        byte[] key = {'k', 'e', 'y'};
+        assertNull(tyrant.get(key));
+    }
 
     @Test
     public void getRetrievesWhatWasPut() throws IOException {
@@ -16,7 +24,43 @@ public class TyrantTest {
         byte[] value = {'v', 'a', 'l', 'u', 'e'};
         tyrant.put(key, value);
         assertArrayEquals(value, tyrant.get(key));
-        disconnected();
+    }
+
+    @Test
+    public void clearDeletesAllValues() throws Exception {
+        byte[] key = {'k', 'e', 'y'};
+        byte[] value = {'v', 'a', 'l', 'u', 'e'};
+        tyrant.put(key, value);
+        tyrant.clear();
+        assertNull(tyrant.get(key));
+    }
+
+    @Test
+    public void removeRemovesKey() throws Exception {
+        byte[] key = {'k', 'e', 'y'};
+        byte[] value = {'v', 'a', 'l', 'u', 'e'};
+        tyrant.put(key, value);
+        tyrant.remove(key);
+        assertNull(tyrant.get(key));
+    }
+
+    @Test
+    public void removeMissingKeyDoesNothing() throws Exception {
+        byte[] key = {'k', 'e', 'y'};
+        tyrant.remove(key);
+    }
+
+    @Test
+    public void emptyMapSizeIsZero() throws Exception {
+        assertEquals(0, tyrant.size());
+    }
+
+    @Test
+    public void oneElementMapSizeIsOne() throws Exception {
+        byte[] key = {'k', 'e', 'y'};
+        byte[] value = {'v', 'a', 'l', 'u', 'e'};
+        tyrant.put(key, value);
+        assertEquals(1, tyrant.size());
     }
 
     @Before
@@ -26,7 +70,8 @@ public class TyrantTest {
     }
 
     @After
-    public void disconnected() throws IOException {
+    public void disconnect() throws IOException {
+        tyrant.clear();
         tyrant.close();
     }
 }
